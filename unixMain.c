1d050248e26f730ddf2a1be616cfc80b7401be55
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-06-03 16:55:52 +0300

    main file

---
+++ unixMain.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+int main()
+{
+    //call the collectData function to start collecting data
+    collectData();
+    return 0;
+    
+}
